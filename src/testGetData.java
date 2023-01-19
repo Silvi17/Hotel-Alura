16553f5d1739cb2b4873d4e577b7a255e30ad5fa
Silvi17 <107277624+Silvi17@users.noreply.github.com>
2023-01-19 02:30:11 -0300

    Add files via upload

---
+++ src/testGetData.java
@@ -0,0 +1,22 @@
+
+
+import controller.HuespedController;
+import controller.ReservaController;
+import dao.HuespedDAO;
+import dao.ReservaDAO;
+import factory.ConnectionFactory;
+
+public class testGetData {
+	public static void main(String[] args) {
+		
+		ReservaController reservaController = new ReservaController();
+		HuespedController huespedController = new HuespedController();
+		
+
+//		System.out.println(reservaController.modificar( "2022-09-01" , "2040-12-31", 1500, "Tarjeta de Crédito", 29));
+
+//		System.out.println(huespedController.modificar());
+		
+	}
+
+}
